3e3ae52e6ef7fe416cfe692531cf17d73544bb14
Ion Dodon <iondodon2@gmail.com>
2022-07-21 22:47:07 +0300

    refactoring

--- src/lib/suggestions/library/commands.ts
+++ src/lib/suggestions/library/commands.ts
@@ -1,36 +1,36 @@
 const files = {
-    script: "ls -a",
-    processor: function (line) {
-        return {
-            'names': [line],
-            'description': 'description of first file',
-            'getNext': function() { return [files] }
-        }
+  script: "ls -a",
+  processor: function (line) {
+    return {
+      'names': [line],
+      'description': 'description of first file',
+      'getNext': function() { return [files] }
     }
+  }
 }
 
 
 const lsOptions = {
-values: [
+  values: [
     {
-        'names': ['-a', '--all'],
-        'description': 'ls all - description',
-        'getNext': function() { return [lsOptions, files] }
+      'names': ['-a', '--all'],
+      'description': 'ls all - description',
+      'getNext': function() { return [lsOptions, files] }
     }
-]
+  ]
 }
 
 export const COMMANDS = {
-    values: [
-        {
-            'names': ['ls'],
-            'description': "ls description",
-            'getNext': function() { return [lsOptions, files] }
-        },
-        {
-            'names': ['sudo'],
-            'description': "super user do",
-            'getNext': function() { return [COMMANDS] }
-        }
-    ]
+  values: [
+    {
+      'names': ['ls'],
+      'description': "ls description",
+      'getNext': function() { return [lsOptions, files] }
+    },
+    {
+      'names': ['sudo'],
+      'description': "super user do",
+      'getNext': function() { return [COMMANDS] }
+    }
+  ]
 }
