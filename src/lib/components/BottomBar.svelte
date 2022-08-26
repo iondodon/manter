46f504b760f5aa77379e41100a2be2af7ca3f6fc
Ion Dodon <iondodon2@gmail.com>
2022-08-26 20:23:24 +0300

    counter

--- src/lib/components/BottomBar.svelte
+++ src/lib/components/BottomBar.svelte
@@ -3,17 +3,35 @@
 </script>
 
 <div id="bottom-bar">
-  Manter (alpha)
+
+  <!-- counterAPI library, insert anywhere on the page. -->
+  <script src="https://counterapi.com/c.js" async ></script>
+  <!-- Add the counter instance somewhere to your template... -->
+  <div class="counterapi" noIcon="true" noCss="true"  ></div>
+  <span> global usages</span>
+
+
+  <span> | </span>
+  <div class="version">
+    Manter (alpha)
+  </div>
 </div>
 
 <style lang="scss">
   #bottom-bar {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+
     bottom: 0em;
     height: 1.2em;
     width: 100%;
-
-    text-align: right;
     background-color: rgb(29, 29, 29);
     color: rgb(156, 156, 156);
+  }
+
+  #bottom-bar * {
+    margin-left: 0.2rem;
+    margin-right: 0.2rem;
   }
 </style>
