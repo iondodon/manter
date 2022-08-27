c0c45c092a548d4949e3796d5d3ca69581b04481
Ion Dodon <iondodon2@gmail.com>
2022-08-27 12:56:12 +0300

    hit counter POST

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
   import { onMount } from 'svelte'
 
   onMount(() => {
-    fetch("http://137.184.207.126:8080/counter/increment")
+    fetch("http://137.184.207.126:8080/counter/increment", { method: 'POST' })
     .then(response => response.json())
     .then(_data => {
     }).catch(_error => {
